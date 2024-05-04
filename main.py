9c9761f7700d79af9df5bf25f0ea22aa51265677
Paola Plebani <139859123+paolaplebani@users.noreply.github.com>
2024-05-04 16:27:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from description_model import describe_image
 from text_to_speech import speak
 from image_from_webcam import take_picture
 
-image = take_picture(res_width = 512, res_height = 512)
+image_path = take_picture(res_width = 512, res_height = 512)
 prompt = transcribe()
-image_description = describe_image(prompt, image)
+image_description = describe_image(prompt, image_path)
 speak(image_description)
