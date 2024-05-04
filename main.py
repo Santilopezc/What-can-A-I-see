a927fa50a65df36e3d14e941ff81009d9249d9f3
Paola Plebani <139859123+paolaplebani@users.noreply.github.com>
2024-05-04 16:10:44 +0200

    Update main.py
    The part with the image capturing from the webcam has been added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from speech_to_text import transcribe
 from description_model import describe_image
 from text_to_speech import speak
+from image_from_webcam import take_picture
 
+image = take_picture(res_width = 512, res_height = 512)
 prompt = transcribe()
 image_description = describe_image(prompt)
 speak(image_description)
